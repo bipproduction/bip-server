488aa1e418912f85ac50de03bf22c98ad7229fdf
bipproduction <bip.production.js@gmail.com>
2023-10-31 16:14:42 +0800

    auto push

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function info() {
     console.log(`
     MENU
     ---------------------
-    \t${_list_menu.map((v) => v.arg + "\t" + v.des+"\n\t*param: "+v.params.join(" ")).join("\t\n\t")}
+    \t${_list_menu.map((v) => v.arg + "\t" + v.des+"\n\t <param> : "+v.params.join(" ")).join("\t\n\t")}
 
     EXAMPLE
         command ${_list_menu[0].arg}
