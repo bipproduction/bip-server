19689d6a49e2a177d6868e9a058ae7b6ad15cc0d
bipproduction <bip.production.js@gmail.com>
2023-10-31 11:25:31 +0800

    auto push

--- src/git_pull/index.js
+++ src/git_pull/index.js
@@ -1,8 +1,11 @@
 const get_current_branch = require("../../lib/get_current_branch")
 const { execSync } = require('child_process')
+require('colors')
 module.exports = function () {
     const branch = get_current_branch()
     execSync(`git pull origin ${branch}`, {
         stdio: "inherit"
     })
+
+    console.log(`SUCCESS`.green)
 }
