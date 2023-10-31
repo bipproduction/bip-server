016ff8b3cfe5ebff4ff5eecbf016eb85a9bc1ac8
bipproduction <bip.production.js@gmail.com>
2023-10-31 10:29:08 +0800

    permulaan

---
+++ index.js
@@ -0,0 +1,38 @@
+const arg = process.argv.splice(2)
+const _ = require('lodash')
+require('colors')
+
+const list_menu = [
+    {
+        id: "menu1",
+        name: "menu1",
+        des: "menu1",
+        arg: "--menu1",
+        act: () => { }
+    },
+    {
+        id: "menu2",
+        name: "menu2",
+        des: "menu2",
+        arg: "--menu2",
+        act: () => { }
+    }
+]
+
+async function info() {
+    console.log(`
+    MENU
+    ---------------------
+    \t${list_menu.map((v) => v.arg + "\t" + v.des).join("\t\n\t")}
+
+    EXAMPLE
+        command ${list_menu[0].arg}
+    `.yellow)
+}
+
+async function main() {
+    if (_.isEmpty(arg)) return info()
+
+}
+
+main()
